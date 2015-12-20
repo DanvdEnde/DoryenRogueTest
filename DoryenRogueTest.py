44242c24892adbb2c29ba3f44cbdfc9295eb693f
JollyJavelin <Danvdende@gmail.com>
2015-12-20 04:16:33 +0100

    Files.

---
+++ DoryenRogueTest.py
@@ -0,0 +1,71 @@
+import libtcodpy as libtcod
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+class Object:
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+def handle_keys():
+    global playerx, playery
+
+    key = libtcod.console_wait_for_keypress(True)
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True
+
+    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        playery -= 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        playery += 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        playerx -= 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        playerx += 1
+
+
+libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Doryen Rogue test', False)
+libtcod.sys_set_fps(LIMIT_FPS)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+playerx = SCREEN_WIDTH/2
+playery = SCREEN_HEIGHT/2
+
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
+
+while not libtcod.console_is_window_closed():
+    for object in objects:
+        object.draw()
+
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    libtcod.console_flush()
+
+    for object in objects:
+        object.clear()
+
+    exit = handle_keys()
+    if exit:
+        break
